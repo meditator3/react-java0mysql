b1869479674d5e913be1c0a158b2736d52247f0e
Ariel <r_e_l_g@yahoo.com>
2024-02-14 22:15:52 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -25,7 +25,7 @@ function App() {
           <p>Loading...</p>
         )}
         <p>
-          Edit <code>src/App.tsx</code> and save to reload. if you think that'll help
+          Edit <code>src/App.tsx</code> and save to reload. if you think that'll help- but it wont.
         </p>
         <a
           className="App-link"
